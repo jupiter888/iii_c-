65dc36be7d4733e1e5df538331911cbc5955c8d1
Daniel <visual.eyes.success@gmail.com>
2025-08-15 14:28:17 +0200

    Update lionsmain.cpp
    compile added for reference

--- lionsmain.cpp
+++ lionsmain.cpp
@@ -1,3 +1,5 @@
+# COMPILE
+# g++-14 -std=c++17 -O3 -march=native -fopenmp \ main.cpp \ $(gdal-config --cflags) $(gdal-config --libs) \ -Wl,-rpath,"$(gdal-config --prefix)/lib" \ -o data_cleaner
 #include <chrono>
 #include <cmath>
 #include <fstream>
